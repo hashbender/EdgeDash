1876f78dff7007f18449003473c48385c5c8f3a3
Nick Hansen <nitronick600@gmail.com>
2015-11-25 23:49:46 -0700

    Fill out Heroku config

--- edgemark_dashboard/settings.py
+++ edgemark_dashboard/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATABASES['default'] =  dj_database_url.config()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -29,6 +31,7 @@ SESSION_COOKIE_SECURE = INPROD
 CSRF_COOKIE_SECURE = INPROD
 CSRF_COOKIE_HTTPONLY = INPROD
 X_FRAME_OPTIONS = "DENY"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = [
     '.edgedash.com'
@@ -119,3 +122,7 @@ LOGOUT_URL = '/logout'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- edgemark_dashboard/wsgi.py
+++ edgemark_dashboard/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edgemark_dashboard.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
