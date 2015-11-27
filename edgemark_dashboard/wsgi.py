b6b47a706bb8416953f8ac202f36cfbd581adbc1
Nick Hansen <nitronick600@gmail.com>
2015-11-27 14:23:26 -0700

    Move settings import order

--- edgemark_dashboard/wsgi.py
+++ edgemark_dashboard/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edgemark_dashboard.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edgemark_dashboard.settings")
 application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
